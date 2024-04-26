fbaa21057b21796b183762d417b2496c97b8e94d
John Patrick <patrickmarasigan06@gmail.com>
2024-04-26 17:38:24 +0800

    done back-end (improvement na lang if gusto)

--- app/blueprints/home.py
+++ app/blueprints/home.py
@@ -1,11 +1,36 @@
-from flask import Blueprint, render_template
-from ..utils.transcript import get_transcript
+from flask import Blueprint, render_template, request
+from ..utils.transcript import *
+from ..utils.summarizer import * 
 
 # Create a blueprint for the home page
 home = Blueprint('home', __name__)
 
-@home.route('/')
+@home.route('/', methods=['POST', 'GET'])
 def home_page():
-    # video_id = "rFltDd3Iab4"
-    # transcript = get_transcript(video_id=video_id)
-    return render_template('home.html')
+    # Log routing
+    log_routing(request)
+
+    summary = []        # empty list to store the summary state
+    if request.method == 'POST': 
+        link = request.form.get('yt-link')
+        if link:
+            video_id = get_video_id(link)
+            transcript = get_transcript(video_id)
+
+            # NLP for summarization
+            word_list = token(transcript)
+            sent_list = sent_token(transcript)
+            filtered_words = remove_stopwords(word_list)
+            word_frequency = word_freq(filtered_words)
+            word_frequency = max_freq(word_frequency)
+            sent_scores = sentence_scores(sent_list, word_frequency)
+            summary = get_summary(sent_scores)
+        else:
+            summary.append('INPUT MUST NOT BE EMPTY')
+
+    return render_template('home.html', summary=summary)
+
+
+def log_routing(request):
+    print("Route: ", request.path)
+    print("Method: ", request.method)
--- app/utils/transcript.py
+++ app/utils/transcript.py
@@ -1,5 +1,30 @@
 from youtube_transcript_api import YouTubeTranscriptApi
 
+# Parse and get the video ID
+def get_video_id(url):
+    # Identifier for youtube video url
+    start_pos = url.find("https://www.youtube.com/watch?v=")
+    end_pos = url.find("&ab_channel=")
+
+    if start_pos == -1 or end_pos == -1:
+        print("Not a valid youtube video link")
+        return None
+
+    video_id = url[start_pos + len("https://www.youtube.com/watch?v="):end_pos]
+
+    return video_id
+
+
+# Get transcript only from dict object
 def get_transcript(video_id):
-    transcript = YouTubeTranscriptApi.get_transcript(video_id=video_id)
-    return transcript
+    all_text = ""   # store all text from transcript
+    transcript = YouTubeTranscriptApi.get_transcript(video_id)
+    
+    # Parse the text only
+    for item in transcript:
+        all_text += item['text'] + " "
+
+    # Remove new line (due to video subtitles positioning)
+    all_text = all_text.replace("\n", " ")
+
+    return all_text
