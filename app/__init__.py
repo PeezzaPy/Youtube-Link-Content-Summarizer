3d50af841a415b6d289a0a3ba71bd8d6e4989d8d
John Patrick <patrickmarasigan06@gmail.com>
2024-05-07 15:37:45 +0800

    added get_started page

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from .blueprints.home import home
+from .blueprints.get_started import get_started
 
+# Register app route
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(home)
+    app.register_blueprint(get_started)
     return app
---
+++ app/blueprints/get_started.py
@@ -0,0 +1,7 @@
+from flask import Blueprint, render_template, request
+
+get_started = Blueprint('get_started', __name__)
+
+@get_started.route("/get_started")
+def get_started_page():
+    return render_template('get_started.html')
