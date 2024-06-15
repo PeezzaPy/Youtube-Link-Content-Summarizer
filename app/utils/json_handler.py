5cbaba6adb4c1abd078ebe698935a5425205e4fa
John Patrick <patrickmarasigan06@gmail.com>
2024-06-16 01:42:57 +0800

    added text file for back-end source code

--- app/utils/json_handler.py
+++ app/utils/json_handler.py
@@ -1,18 +1,18 @@
 import json
 
-# Create json format of youtube link video info
-def make_yt_info_json(response, video_id):
-    filename = f'json_yt_info/{video_id}.json'
-    with open(filename, 'w') as f:
-        json.dump(response, f, indent=4)
+# # Create json format of youtube link video info
+# def make_yt_info_json(response, video_id):
+#     filename = f'json_yt_info/{video_id}.json'
+#     with open(filename, 'w') as f:
+#         json.dump(response, f, indent=4)
 
-    with open(filename, 'r') as f:
-        file_content = json.load(f)
+#     with open(filename, 'r') as f:
+#         file_content = json.load(f)
 
-    if file_content == response:
-        print(f"Checksum passed {filename}")
-    else:
-        print(f"Checksum failed {filename}")
+#     if file_content == response:
+#         print(f"Checksum passed {filename}")
+#     else:
+#         print(f"Checksum failed {filename}")
 
 
 # Retrieve youtube api key
